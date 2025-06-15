f228d6666b190fa3eaa08c8c4ce672bdaec96122
Zobachi <chizobachristiana17@gmail.com>
2025-06-15 07:13:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
+import userRoute from './routes/userRoute.js';
 import movieRoutes from './routes/movieRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
